f3ae4c21f3f3ab47c11189a93a2ebff40061af81
Roman Rybachok <72809424+romamam@users.noreply.github.com>
2025-01-27 15:24:47 +0200

    Initial commit

---
+++ src/test/java/com/project/petproject/PetprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.petproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
