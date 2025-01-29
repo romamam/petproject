c64f66ccd222f48fc71cd72df855a6b3b41f1275
Roman Rybachok <72809424+romamam@users.noreply.github.com>
2025-01-29 21:44:38 +0200

    Fix spelling mistake in PetprojectApplication

--- src/main/java/com/project/petproject/PetProjectApplication.java
+++ src/main/java/com/project/petproject/PetProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetprojectApplication {
+public class PetProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetprojectApplication.class, args);
+		SpringApplication.run(PetProjectApplication.class, args);
 	}
 
 }
